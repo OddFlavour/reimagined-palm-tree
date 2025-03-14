df86e8c163a5f363d5d7e7324d589734c55a5f8a
OddFlavour <kavkeon@gmail.com>
2025-03-14 23:05:36 +0000

    Fix GitHub pages integration again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reimagined-palm-tree/",  // Used for GitHub pages deployment
+  base: "/reimagined-palm-tree",  // Used for GitHub pages deployment
   server: {
     host: '127.0.0.1',  // Container defaults to IPv6 when specifying "localhost", so manually use IPv4 address instead
     watch: {
